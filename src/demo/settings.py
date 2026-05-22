7edccfe7b59a42ba98edb728e8223cacbd3fb7c2
michaelcriss <work.crisostomo@gmail.com>
2026-05-22 14:52:55 -0400

    add test vars

--- src/demo/settings.py
+++ src/demo/settings.py
@@ -8,7 +8,7 @@ env = environ.Env()
 env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/a
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY')
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
+TEST_OUTPUT_DIR = 'test-results'
+TEST_OUTPUT_VERBOSE = 2
